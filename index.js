cad861af422f3413e4aa1b8d87f8a2c1ef1f520a
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-03-30 20:52:53 +0300

    ready

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+// Return first two drivers
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+// Return last two drivers
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+// Array containing the two functions
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// Higher-order function to create fare multiplier
+const createFareMultiplier = function(integer) {
+  return function(fare) {
+    return fare * integer;
+  };
+};
+
+// Fare doubler function
+const fareDoubler = createFareMultiplier(2);
+
+// Fare tripler function
+const fareTripler = createFareMultiplier(3);
+
+// Select different drivers based on function provided
+const selectDifferentDrivers = function(drivers, func) {
+  return func(drivers);
+};
+
+module.exports = {
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers,
+  selectingDrivers,
+  createFareMultiplier,
+  fareDoubler,
+  fareTripler,
+  selectDifferentDrivers
+};
